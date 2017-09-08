939c602f4a7bedc6805355f889ab373e69cc45db
colesayer <colesayershapiro@gmail.com>
2017-09-08 11:13:18 -0400

    needs single source of truth

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+require 'pry'
+class Author
 
+  attr_accessor :name, :books
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.most_words
+  end
+
+  def books
+    @books
+  end
+
+  def write_book(title, words)
+    book = Book.new(title, words, self)
+    self.books << book
+  end
+
+  def total_words
+    @books.collect do |book|
+      book.words
+    end.inject(0){|sum,x| sum + x }
+  end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,30 @@
+class Book
 
+  attr_accessor :title, :author, :words
+
+  @@all = []
+
+  def initialize(title, words, author)
+    @title = title
+    @words = words
+    @author = author
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author
+    self.author
+  end
+
+  def title
+    self.title
+  end
+
+  def word_count
+    self.words
+  end
+
+end
